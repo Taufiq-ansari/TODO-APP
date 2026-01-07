6fb7f6f303c34df2adce55e970d49aba414f2f25
Bhumika Patel <bhumika.patel@macymind.in>
2026-01-07 14:45:43 +0530

    added formatter in analysis_option.yaml

--- lib/todohomepage.dart
+++ lib/todohomepage.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ToDoHomepage extends StatelessWidget {
-   ToDoHomepage({super.key});
+  ToDoHomepage({super.key});
 
   // store a value
   List storeValue = [];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("TODO-APP")));
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("TODO-APP"),
+      ),
+    );
   }
 }
